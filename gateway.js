15ec2de4b856c30ca9577a896019573a6beee296
Christiaan Hougaard <cwhougaard@gmail.com>
2021-06-13 12:53:05 -0400

    created the basic route setup for the gateway

--- gateway.js
+++ gateway.js
@@ -1,10 +1,14 @@
 const express = require('express')
 require('dotenv').config()
+const routes = require('./routes/routes')
 
 const port = process.env.PORT || 5001
 
 const app = express()
 
+app.use(express.json())
+
+app.use('/', routes)
 
 app.listen(port, () =>{
   console.log(`Gateway successfully running on http://www.localhost:${port}`);
