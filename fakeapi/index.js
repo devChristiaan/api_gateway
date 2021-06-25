a6cb3f28dce77a7575dca12effae7fc7aa7ade7d
Christiaan Hougaard <cwhougaard@gmail.com>
2021-06-25 18:36:50 -0400

    set up the first dynamic route and tested it with a simple get request

--- fakeapi/index.js
+++ fakeapi/index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 //Routes
 app.get('/fakeapi', (req, res, next) =>{
-  res.send("hello from fake api server")
+  res.send("hello from fake api server agian")
 })
 
 //Server
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 //Imports
 const express = require("express")
 const router = express.Router()
+const axios = require("axios")
 
 //Grab the api name and forward the request
 router.all('/:apiName', (req, res) =>{
-  res.send(console.log('hello world'))
+  axios.get(`http://localhost:9001/${req.params.apiName}`)
+  .then( response => {res.send(response.data)})
 })
 
 //Exports
